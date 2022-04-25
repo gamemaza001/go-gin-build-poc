15212b50a83cdccee9800f43403834adae39bee0
Tanakorn <tanakorn.mengweha@scb.co.th>
2022-04-25 23:42:42 +0700

    set max idle conns

--- main.go
+++ main.go
@@ -57,8 +57,8 @@ func main() {
 			return
 		}
 		db.SetConnMaxLifetime(0)
-		db.SetMaxOpenConns(20)
-		db.SetMaxIdleConns(10)
+		db.SetMaxOpenConns(10)
+		db.SetMaxIdleConns(5)
 
 		insertStatement := `INSERT INTO books (Name, Age) VALUES ($1, $2)`
 
