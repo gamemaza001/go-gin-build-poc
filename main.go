1acb44e8566a420f3fd7b1a1744b0c3b7fcdf0be
Tanakorn <tanakorn.mengweha@scb.co.th>
2022-04-25 11:34:08 +0700

    set max idle conns

--- main.go
+++ main.go
@@ -56,8 +56,8 @@ func main() {
 			})
 			return
 		}
-		db.SetMaxOpenConns(3)
-		db.SetMaxIdleConns(3)
+		db.SetMaxOpenConns(5)
+		db.SetMaxIdleConns(5)
 		// db.SetMaxOpenConns(25)
 		// db.SetMaxIdleConns(25)
 
